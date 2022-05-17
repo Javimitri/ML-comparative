537eedea38a0fdf5958fe12c489feb170e5af8bf
fjperezv_bec <fjperezv.bec@acciona.com>
2022-05-17 09:44:39 +0200

    actualizacion

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import LinearRegression
 
 
 
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
